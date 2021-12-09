7c97ae30901e46165bf0475db6b5d211eadb15f6
Helena Merk <helena.lmtm@gmail.com>
2021-12-09 14:19:10 -0800

    solana wallet connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import { FC, ReactNode } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+// Use require instead of import, and order matters
+require('../styles/globals.css');
+require('@solana/wallet-adapter-react-ui/styles.css');
 
+const WalletConnectionProvider = dynamic<{ children: ReactNode }>(
+  () =>
+    import('../components/WalletConnectionProvider').then(
+      ({ WalletConnectionProvider }) => WalletConnectionProvider
+    ),
+  {
+    ssr: false,
+  }
+);
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <WalletConnectionProvider>
+      <WalletModalProvider>
+        <Component {...pageProps} />
+      </WalletModalProvider>
+    </WalletConnectionProvider>
+  );
+};
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
+import {
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from '@solana/wallet-adapter-react-ui';
 
 const Home: NextPage = () => {
-  return <div className="text-xl">cool</div>;
+  return (
+    <div className="text-xl">
+      <div>
+        <WalletMultiButton />
+        <WalletDisconnectButton />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
