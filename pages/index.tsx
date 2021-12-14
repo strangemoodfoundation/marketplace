2d7bbd685e9900756d2c9e7500f3341afa5e54fa
Helena Merk <helena.lmtm@gmail.com>
2021-12-13 23:02:14 -0800

    merge fixes and cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,7 @@ import {
 } from '@solana/wallet-adapter-react-ui';
 import { SendOneLamportToRandomAddress } from '../components/SendLamportExample';
 
-const sendLamportExample = () => {};
-
-import Strangemood from '@strangemood/strangemood';
 import type { NextPage } from 'next';
-import * as solana from '@solana/web3.js';
-import { useRouter } from 'next/router';
 import { useFlag } from '../lib/useFlag';
 import { CreateWrappedNativeAccountExample } from '../components/CreateWrappedNativeAccountExample';
 import { GetAccountListingsExample } from '../components/GetAccountListingExample';
@@ -18,15 +13,6 @@ import { PurchaseListingExample } from '../components/PurchaseListingExample';
 
 const Home: NextPage = () => {
   const network = useFlag('network', 'mainnet-beta');
-
-  // function onMakeNewListing() {
-  //   const conn = new solana.Connection(solana.clusterApiUrl(network));
-  //   // Strangemood.client.createListing(
-  //   //   conn,
-  //   //   Strangemood.MAINNET.STRANGEMOOD_PROGRAM_ID,
-  //   //   {}
-  //   // );
-  // }
 
   return (
     <div className="text-xl">
