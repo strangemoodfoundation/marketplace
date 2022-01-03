13701d6ed003ad6ff2360bf11a1cc4701b659d23
Evan Conrad <evan@roomservice.dev>
2022-01-03 09:11:19 -0800

    user wallet adapter css manually

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/wallet-adapter.css';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
