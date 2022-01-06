e5ed8c63a658ed23b49f3918b071e3c1a03baa7b
Evan Conrad <evan@roomservice.dev>
2022-01-06 11:05:29 -0800

    add cors here

--- pages/api/pin/[pubkey].tsx
+++ pages/api/pin/[pubkey].tsx
@@ -9,10 +9,7 @@ import Cors from 'cors';
 // Initialize the cors middleware
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-  })
+  Cors()
 );
 
 function dummyWallet() {
